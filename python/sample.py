4683f9ed2cd3018a3aa6310b35d6a358f9bdcdb2
dmlerner <dmlerner@gmail.com>
2020-12-13 16:09:12 -0800

    .

--- python/sample.py
+++ python/sample.py
@@ -5,7 +5,9 @@ def rotate_windows(vim):
     print(vim.current.window)
     print(vim.windows)
     #del vim.current.buffer[-1]
-    print(vim.current.window.vars)
-    print(vars(vim.current.window.vars))
+    #print(vim.current.window.vars)
+    #print(vars(vim.current.window.vars))
+    print(vim.current.tabpage.windows)
+    vim.current.tabpage.window.width += 10
 
     print("rotating")
