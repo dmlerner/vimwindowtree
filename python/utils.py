9024bb08bbf3dc8f0cfe4418d3b624db4a42f768
David Lerner <davidlerner@google.com>
2020-12-15 11:40:35 -0800

    add resize, round

--- python/utils.py
+++ python/utils.py
@@ -1,40 +1,63 @@
 import operator
 import pdb
 
-def add(sizes, size):
+def dbg():
+    #pdb.set_trace()
+    pass
+
+def resize(sizes, size):
     total_before = sum(sizes)
-    total_after = total_before - size - 1 # window divider necessitates - 1
-    ratio = total_after / total_before
-    #return *(s*ratio for s in sizes), size
+    ratio = size / total_before
     return *map(ratio.__mul__, sizes), size
 
-def rounded_add(sizes, size):
-    unrounded = add(sizes, size)
-    print('unrounded', unrounded)
-    rounded = tuple(map(int, map(round, unrounded)))
-    error = sum(sizes) - 1 - sum(rounded)
+
+
+def add(sizes, size):
+    return resize((*sizes, size), sum(sizes)-1)
+    #total_before = sum(sizes)
+    #total_after = total_before - size - 1 # window divider necessitates - 1
+    #ratio = total_after / total_before
+    ##return *(s*ratio for s in sizes), size
+    #return *map(ratio.__mul__, sizes), size
+
+def my_round(unrounded):
+    dbg()
+    print('unrounded', unrounded, sum(unrounded))
+    rounded = list(map(int, map(round, unrounded)))
+    error = sum(rounded) - sum(unrounded)
 
     if not error:
-        return rounded
+        return tuple(rounded)
 
     sign = 1 if error > 0 else - 1
     i = 0
     while error:
-        print('rounded while error', rounded)
+        print('rounded while error', rounded, error)
         term_error = rounded[i] - unrounded[i]
         if  term_error * error > 0: # same sign
             rounded[i] -= sign
+            error -= sign
         i = (i + 1) % len(rounded)
+        if i == 0:
+            print('errorrrrrr')
+            break # for testing
 
     print('returning', rounded)
-    return rounded
+    return tuple(rounded)
+
+def rounded_add(sizes, size):
+    dbg()
+    unrounded = add(sizes, size)
+    return my_round(unrounded)
+
 
 sizes = 2,4
 size = 1
 print(sizes, size)
-print('->', add(sizes, size), rounded_add(sizes, size))
+#print('->', add(sizes, size), rounded_add(sizes, size))
 
-def test_assert(expected, actual):
+def test_assert(actual, expected):
+    print('...')
     try:
         assert expected == actual
     except:
@@ -42,3 +65,4 @@ def test_assert(expected, actual):
 
 test_assert(rounded_add((2,2), 1),(1, 1, 1))
 test_assert(rounded_add((2,4), 1),(1, 3, 1))
+
