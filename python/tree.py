0aae108438c6ba1dcfcaa2c7c5ceef316b84b6f5
david lerner <dmlerner@gmail.com>
2020-12-14 21:13:02 -0800

    add tree

--- python/tree.py
+++ python/tree.py
@@ -36,14 +36,6 @@ class WindowNode:
     def get_siblings(self):
         return list(filter(lambda x: x is not self, self.parent.children))
 
-    def window_size(self, orientation):
-        # TODO does this work with mutating?
-        assert not self.is_container()
-        return { HORIZONTAL: self.window.width, VERTICAL: self.window.height }[orientation]
-
-    def set_size(self, orientation):
-        assert not self.is_container()
-
     def total_size(self, orientation):
         if self.is_container():
             return sum(child.total_size(orientation) for child in self.children)
@@ -55,7 +47,7 @@ class WindowNode:
     def resize_absolute(self, delta, orientation):
         ''' Increase the size of this window, or the total size of all its children, by delta, by decreasing the size of its siblings. '''
 
-        total_sibling_size_before = sum(wn.total_size() for wn in self.get_siblings())
+        total_sibling_size_before = sum(wn.width for wn in self.get_siblings())
         assert not self.is_container() # TODO
         for child in self.parent.children:
             # TODO: do I need to do siblings before me, then me, then those after me?
