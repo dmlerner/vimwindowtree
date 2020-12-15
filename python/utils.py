f4d582a68fe61a1ae28d1f2ed5ec9ede4cb6274b
David Lerner <davidlerner@google.com>
2020-12-15 11:55:48 -0800

    min_error

--- python/utils.py
+++ python/utils.py
@@ -2,51 +2,51 @@ import operator
 import pdb
 
 def dbg():
-    #pdb.set_trace()
+    pdb.set_trace()
     pass
 
 def resize(sizes, size):
     total_before = sum(sizes)
     ratio = size / total_before
-    return *map(ratio.__mul__, sizes), size
+    return tuple(map(ratio.__mul__, sizes))
 
 
 
 def add(sizes, size):
     return resize((*sizes, size), sum(sizes)-1)
-    #total_before = sum(sizes)
-    #total_after = total_before - size - 1 # window divider necessitates - 1
-    #ratio = total_after / total_before
-    ##return *(s*ratio for s in sizes), size
-    #return *map(ratio.__mul__, sizes), size
 
 def my_round(unrounded):
-    dbg()
-    print('unrounded', unrounded, sum(unrounded))
+    # round, ensuring nearly unaltered sum
     rounded = list(map(int, map(round, unrounded)))
     error = sum(rounded) - sum(unrounded)
+    # returning integers, so may not be possible to have zero error
+    min_error = abs(sum(unrounded) - round(sum(unrounded)))
 
     if not error:
         return tuple(rounded)
 
     sign = 1 if error > 0 else - 1
     i = 0
-    while error:
-        print('rounded while error', rounded, error)
-        term_error = rounded[i] - unrounded[i]
-        if  term_error * error > 0: # same sign
-            rounded[i] -= sign
+    while abs(error) > min_error*1.01: # float precision
+        if i > 10:
+            break
+        #print('rounded while error', rounded, error)
+        j = i % len(rounded)
+        print(i, j, error)
+        term_error = rounded[j] - unrounded[j]
+        if  i != j or term_error * error > 0: # same sign
+            rounded[j] -= sign
             error -= sign
-        i = (i + 1) % len(rounded)
-        if i == 0:
-            print('errorrrrrr')
-            break # for testing
 
-    print('returning', rounded)
+        i += 1
+        #if i == 0:
+            #print('errorrrrrr')
+            #break # for testing
+
+    #print('returning', rounded)
     return tuple(rounded)
 
 def rounded_add(sizes, size):
-    dbg()
     unrounded = add(sizes, size)
     return my_round(unrounded)
 
@@ -65,4 +65,4 @@ def test_assert(actual, expected):
 
 test_assert(rounded_add((2,2), 1),(1, 1, 1))
 test_assert(rounded_add((2,4), 1),(1, 3, 1))
-
+test_assert(my_round((1.3, 1.3, 1.3)), (2, 1, 1))
